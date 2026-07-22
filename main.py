c151b67d3a434631c37c8b41a190d7b42919dfff
inuwara1230 <inuwaraosun@gmail.com>
2026-07-22 21:34:12 +0530

    initial commit

---
+++ main.py
@@ -0,0 +1,25 @@
+import time
+import neopixel
+import machine
+
+ledpin = machine.Pin(16, machine.Pin.OUT)
+pixels = neopixel.NeoPixel(ledpin, 1)
+
+c= True
+while c:
+    pixels[0] = (255, 0, 0)  
+    pixels.write()  
+    time.sleep(1) 
+
+    pixels[0] = (0, 255, 0)  
+    pixels.write()  
+    time.sleep(1)  
+
+    pixels[0] = (0, 0, 255)  
+    pixels.write()  
+    time.sleep(1)  
+
+    pixels[0] = (0,0,0)  
+    pixels.write()  
+    time.sleep(1)
+    c = False  

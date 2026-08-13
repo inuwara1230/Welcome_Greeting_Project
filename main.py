8fcd39f56c909d9ea8083b98460ae1ab8910597b
inuwara1230 <inuwaraosun@gmail.com>
2026-08-13 23:03:18 +0530

    real welcome audio  project code

--- main.py
+++ main.py
@@ -1,26 +1,46 @@
 import time
 import neopixel
-import machine
+import os
+from machine import I2S,Pin
 
-ledpin = machine.Pin(16, machine.Pin.OUT)
-pixels = neopixel.NeoPixel(ledpin, 1)
+#ledpin = machine.Pin(16, machine.Pin.OUT)
+#pixels = neopixel.NeoPixel(ledpin, 1)
 
-c= True
+BCLK_PIN = 10
+LRC_PIN = 11
+DATA_PIN = 12
 
-while c:
-    pixels[0] = (255, 0, 0)  
-    pixels.write()  
-    time.sleep(1) 
+audio_out = I2S(
+    0,
+    sck=Pin(BCLK_PIN),
+    ws=Pin(LRC_PIN),
+    sd=Pin(DATA_PIN),
+    mode=I2S.TX,
+    bits=16,
+    format=I2S.MONO,
+    rate=48000,
+    ibuf=20000
+)
 
-    pixels[0] = (0, 255, 0)  
-    pixels.write()  
-    time.sleep(1)  
+#print("I2S initialized")
 
-    pixels[0] = (0, 0, 255)  
-    pixels.write()  
-    time.sleep(1)  
+print(f"files: {os.listdir()}")
+time.sleep(5)
 
-    pixels[0] = (0,0,0)  
-    pixels.write()  
-    time.sleep(1)
-    c = False  
+try:
+    with open("cs1.wav", "rb") as f:
+        #print("file loaded")
+        
+        f.seek(44)
+
+        while True:
+            data = f.read(1024)
+            if not data:
+                break
+            audio_out.write(data)
+    #print("Playback finished")
+except Exception as e:
+    print(f"An error occurred: {e}")
+
+finally:
+    audio_out.deinit()
